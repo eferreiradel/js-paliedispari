481bb128c4a7b045ad066f96502bf6f50c1eb0ff
eferreiradel <cont.limaferreira.everton@gmail.com>
2023-05-15 17:58:45 +0200

    feat Palindromo

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
-let btnPalindromo = document.querySelector("#btnSubmit");
-btnPalindromo.addEventListener("click", inputValue);
+let result = document.querySelector("#result");
 
 let palindromo = document.querySelector("#inputPalindromo");
 
+console.log("sadad");
+
+document.querySelector("#btnSubmit").addEventListener("click", () => {
+  const resultContainer = document.querySelector("#result");
+  const word = inputValue();
+  const wordReversed = reverseString(word);
+
+  if (word == wordReversed) {
+    result.innerHTML = "Palindoma!";
+    console.log("palindorma");
+  } else {
+    result.innerHTML = "No :(";
+  }
+});
+
+function reverseString(string) {
+  return string.split("").reverse().join("");
+}
+
 function inputValue() {
   let formInputValue = palindromo.value;
-  console.log(formInputValue);
+  return formInputValue;
 }
